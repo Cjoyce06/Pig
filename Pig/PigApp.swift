46d7618d5dc9b24a327c7f06bb67def99c08ff66
Colin Joyce <Cjoyce1@bsd220.org>
2022-11-17 16:05:28 -0600

    Initial Commit

---
+++ Pig/PigApp.swift
@@ -0,0 +1,17 @@
+//
+//  PigApp.swift
+//  Pig
+//
+//  Created by Colin Joyce on 11/17/22.
+//
+
+import SwiftUI
+
+@main
+struct PigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
